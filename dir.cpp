8b757d37a6c6e23cd0fe805a7bcd0da427371c6c
nguyenngocbaocmt02 <80208984+nguyenngocbaocmt02@users.noreply.github.com>
2021-03-17 21:49:39 +0700

    Add files via upload

---
+++ dir.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<dirent.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Input path: ";
+    string path;
+    cin >> path;
+    DIR* dir;
+    dirent* pdir;
+    vector<string> files;
+    dir = opendir(path.c_str());
+    while (pdir = readdir(dir)) {
+        files.push_back(pdir->d_name);
+    }
+    for(int i = 0; i < files.size(); ++i){
+        cout << files[i] << '\n';
+    }
+    system("pause");
+    return 0;
+}
